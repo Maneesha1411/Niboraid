e5b4191485560638451e09feefdbd99394f9c81b
Maneesha1411 <guttamaneesha456@gmail.com>
2026-09-10 08:37:11 +0530

    fix cors for vercel deployment

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://niboraid.vercel.app",
+        "https://niboraid-8kw3arzs1-guttamaneesha456-4505.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
